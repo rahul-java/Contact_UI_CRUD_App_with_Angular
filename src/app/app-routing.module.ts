0138f4b238d34ab7b38688d057a371d22e530151
Rahul Kumar Pandey <rp201291@gmail.com>
2023-09-30 09:12:17 +0530

    contat ui crud app commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CreatecontactComponent } from './createcontact/createcontact.component';
+import { ContacteditComponent } from './contactedit/contactedit.component';
+import { ContactlistComponent } from './contactlist/contactlist.component';
+
+const routes: Routes = [
+  {path:'',redirectTo:'contacts',pathMatch:'full'},
+  {path:'create-contact',component:CreatecontactComponent},
+  {path:'edit/:id',component:ContacteditComponent},
+  {path:'contacts',component:ContactlistComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
